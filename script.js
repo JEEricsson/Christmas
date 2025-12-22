1401e770d5dccf86e8b9d5a0c7a93739b5228be0
Jean-Erik <jean.salmensuu@gmail.com>
2025-12-22 18:56:16 +0100

    Added snow

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// Start snowfall when page loads
+window.addEventListener('load', startSnowfall)
+
 let section = document.querySelector('section')
 let j = 0
 while (j < 180) {
@@ -17,4 +20,35 @@ while (j < 180) {
 
   section.appendChild(star)
   j++
+}
+
+function createSnowflake() {
+  const snowflake = document.createElement('div')
+  snowflake.classList.add('snowflake')
+
+  // Random horizontal position
+  snowflake.style.left = Math.random() * 100 + 'vw'
+
+  // Random size between 2 and 10 pixels
+  const size = Math.random() * 8 + 2
+  snowflake.style.width = `${size}px`
+  snowflake.style.height = `${size}px`
+
+  // Random fall duration between 3 and 10 seconds
+  snowflake.style.animationDuration = `${Math.random() * 7 + 3}s`
+
+  // Random opacity
+  snowflake.style.opacity = Math.random()
+
+  section.appendChild(snowflake)
+
+  // Remove snowflake after it falls
+  setTimeout(() => {
+    snowflake.remove()
+  }, 10000)
+}
+
+// Create snowflakes periodically
+function startSnowfall() {
+  setInterval(createSnowflake, 100)
 }
